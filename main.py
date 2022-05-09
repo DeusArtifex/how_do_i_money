6d0afbb219c10a10fc511aab41603f4323959613
DeusArtifex <deus3artifex@gmail.com>
2022-05-09 23:33:09 +0200

    Create basic window

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def button_clicked(event):
+    print('Button clicked')
+
+
+root = tk.Tk()
+root.title('How Do I Money??')
+root.iconbitmap('./assets/appicon.ico')
+
+W_WID = 800
+W_HEI = 400
+
+s_wid = root.winfo_screenwidth()
+s_hei = root.winfo_screenheight()
+
+center_x = int(s_wid / 2 - W_WID / 2)
+center_y = int(s_hei / 2 - W_HEI / 2)
+
+root.geometry(f'{W_WID}x{W_HEI}+{center_x}+{center_y}')
+root.resizable(False, False)
+
+message = ttk.Label(root, text="Hello there").pack()
+
+button = ttk.Button(root, text='Submit')
+button.bind('<Return>', button_clicked)
+button.bind('<Button>', button_clicked)
+button.focus()
+button.pack()
+
+root.mainloop()
+
